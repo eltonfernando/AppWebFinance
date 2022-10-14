8e4da0a2e85601c9d372054430ec304f2ebc8467
eltonfernando <eltonfernando90@gmail.com>
2022-10-14 07:37:23 -0400

    add pages

---
+++ src/pages/List/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {Container} from './styled'
+import ContentBase from "../../components/Content";
+const List: React.FC = () => {
+
+    return(
+        <Container>
+        <ContentBase/>
+    </Container>
+    )
+}
+
+export default List;
---
+++ src/pages/dashbord/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container } from "./styled";
+
+import ContentBase from './../../components/Content'
+
+
+const Dashbord: React.FC = () => {
+
+
+    return(
+        <Container>
+            <ContentBase/>
+        </Container>
+
+    )
+}
+
+export default Dashbord;
