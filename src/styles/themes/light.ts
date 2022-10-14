0ba4185520c9c7438de3d99eb6a7101550560805
eltonfernando <eltonfernando90@gmail.com>
2022-10-14 07:39:57 -0400

    styles

---
+++ src/styles/themes/light.ts
@@ -0,0 +1,18 @@
+export default {
+    title: 'light',
+    colors: {
+        primary: '#DCDCDC',
+        secondary: '#FFFF',
+        tertiary: '#F5F5F5',
+
+        white: '#000',
+        black: '#FFF',
+        gray: '#BFBFBF',
+
+        success: '#03BB85',
+        info: '#3B5998',
+        warning: '#FF6961'
+    
+        
+    }
+}
