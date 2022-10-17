cb1a1e55a2b2250881b45ed7e1f06fc2da00d069
eltonfernando <eltonfernando90@gmail.com>
2022-10-17 09:54:13 -0400

    add component aside

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,48 +1,94 @@
-import React from "react";
+import React, {useState} from 'react';
+import Toggle from '../Toggle';
+
 import {
     MdDashboard,
     MdArrowDownward,
     MdArrowUpward,
-    MdExitToApp
-} from  'react-icons/md';
+    MdExitToApp,
+    MdClose,
+    MdMenu, 
+} from 'react-icons/md';
+
 import logoImg from '../../assets/logo.svg';
 
-import {Container,
-        Header,
-        LogImg,
-        Title,
-        MenuContainer,
-        MenuItemLink
-        } from './styles';
+import { useAuth } from '../../hooks/auth';
+import { useTheme } from '../../hooks/theme';
 
-const Aside : React.FC = () => {
+import { 
+    Container,
+    Header,
+    LogImg,
+    Title,
+    MenuContainer,
+    MenuItemLink,
+    MenuItemButton,
+    ToggleMenu,
+    ThemeToggleFooter,
+}  from './styles';
 
-    return(
-        <Container>
-        <Header>
-            <LogImg src={logoImg} alt={"Logo Minha carteira"}/>
-            <Title>Minha carterira</Title>
-        </Header>
-        <MenuContainer>
-            <MenuItemLink href="#">
-                <MdDashboard/>
-                Dashbord
-            </MenuItemLink>
-            <MenuItemLink href="#">
-                <MdArrowUpward/>
-                Entradas
-            </MenuItemLink>
-            <MenuItemLink href="#">
-                <MdArrowDownward/>
-                Saídas
+const Aside: React.FC = () => {
+    const { signOut } = useAuth();
+    const { toggleTheme, theme } = useTheme();
+
+    const [toggleMenuIsOpened, setToggleMenuIsOpened ] = useState(false);
+    const [darkTheme, setDarkTheme] = useState(() => theme.title === 'dark' ? true : false);
+
+
+    const handleToggleMenu = () => {
+        setToggleMenuIsOpened(!toggleMenuIsOpened);
+    }
+
+
+    const handleChangeTheme = () => {
+        setDarkTheme(!darkTheme);
+        toggleTheme();
+    }
+
+
+    return (
+        <Container menuIsOpen={toggleMenuIsOpened}>
+            <Header>
+                <ToggleMenu onClick={handleToggleMenu}>
+                { toggleMenuIsOpened ? <MdClose /> : <MdMenu /> }
+                </ToggleMenu>
+
+                <LogImg src={logoImg} alt="Logo Minha Carteira" />
+                <Title>Minha Carteira</Title>
+            </Header>
+
+            <MenuContainer>
+                <MenuItemLink href="/">
+                    <MdDashboard />
+                    Dashboard
                 </MenuItemLink>
-            <MenuItemLink href="#">
-                <MdExitToApp/>
-                Sair
+
+                <MenuItemLink href="/list/entry-balance">
+                    <MdArrowUpward />
+                    Entradas
                 </MenuItemLink>
-        </MenuContainer>
+
+                <MenuItemLink href="/list/exit-balance">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+
+                <MenuItemButton onClick={signOut}>
+                    <MdExitToApp />
+                    Sair
+                </MenuItemButton>
+            </MenuContainer>
+
+            <ThemeToggleFooter menuIsOpen={toggleMenuIsOpened}>
+                <Toggle
+                    labelLeft="Light"
+                    labelRight="Dark"
+                    checked={darkTheme}
+                    onChange={handleChangeTheme}
+                />
+            </ThemeToggleFooter>
         </Container>
-    )
+    );
 }
 
 export default Aside;
